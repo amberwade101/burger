347d33a4f7ef13f1eea5072a6705aa23c6463af5
amber wade <amberwade@ambers-MacBook-Pro.local>
2018-06-27 08:19:52 -0700

    added2

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,13 @@
 var mysql = require("mysql");
-var connection;
 
-if(process.env.JAWSDB_URL){
-var connection = mysql.createConnection(process.env.JAWSDB_URL);} else
-{
-  connection =mysql.createConnection({
+var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
   user: "root",
   password: "root",
   database: "burgers_DB"
 });
-}
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
